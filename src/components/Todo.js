4a387ef7d15104232fc91a79475935e17dd7a1a2
Rita Bradley <contact@ritabradley.com>
2020-05-19 10:01:40 -0700

    feat: Line through completed items
    - Add conditional logic to class names to cross out completed items
    - Add new property to todo item call 'isCompleted'

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Todo = ({ todo }) => (
-    <div className='py-4 px-6 my-4 shadow-md border-b-2 border-r-2 rounded-md bg-red-100'>{todo.text}</div>
+const Todo = ({ todo, index, completeTodo }) => (
+    <div
+        className={
+            todo.isCompleted
+                ? 'py-4 px-6 my-4 shadow-md border-b-2 border-r-2 rounded-md bg-red-100 flex justify-between line-through'
+                : 'py-4 px-6 my-4 shadow-md border-b-2 border-r-2 rounded-md bg-red-100 flex justify-between'
+        }
+    >
+        {todo.text}
+        <div>
+            <button
+                className='rounded-md py-2 px-3 bg-green-600 text-green-100 hover:bg-green-800'
+                onClick={() => completeTodo(index)}
+            >
+                Complete
+            </button>
+        </div>
+    </div>
 );
 
 export default Todo;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,21 +3,24 @@ import Todo from '../components/Todo';
 import TodoForm from '../components/TodoForm';
 
 function App() {
-    const [todos, setTodos] = useState([
-        { text: 'Learn about React Hooks' },
-        { text: 'Take a nap' },
-        { text: 'Build todo app' },
-    ]);
+    const [todos, setTodos] = useState([{ text: 'Learn about React Hooks', isCompleted: false }]);
 
     const addTodo = (text) => {
         const newTodos = [...todos, { text }];
         setTodos(newTodos);
     };
+
+    const completeTodo = (i) => {
+        const newTodos = [...todos];
+        newTodos[i].isCompleted = true;
+        setTodos(newTodos);
+    };
+
     return (
         <div className=' flex justify-center'>
             <div className='p-8 w-1/2'>
                 {todos.map((todo, i) => (
-                    <Todo key={i} index={i} todo={todo} />
+                    <Todo key={i} index={i} todo={todo} completeTodo={completeTodo} />
                 ))}
                 <TodoForm addTodo={addTodo} />
             </div>
